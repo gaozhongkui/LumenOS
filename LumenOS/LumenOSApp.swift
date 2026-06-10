f4a6a437525e0d01890713b13fa08c6caecb3815
gaozhongkui <gaozhongkui@meetstarinc.com>
2026-06-10 10:10:12 +0800

    init project

---
+++ LumenOS/LumenOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumenOSApp.swift
+//  LumenOS
+//
+//  Created by gaozhongkui on 2026/6/10.
+//
+
+import SwiftUI
+
+@main
+struct LumenOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
